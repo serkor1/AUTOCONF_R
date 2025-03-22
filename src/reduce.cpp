32af65facb242f61b03628a5546930a8f15a293e
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-03-22 06:50:39 +0100

    :hammer: Changing the standard
    * The standard directly controls where the function is executed - EPIIIIIIIIC!!!

--- src/reduce.cpp
+++ src/reduce.cpp
@@ -5,7 +5,25 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 Rcpp::IntegerVector iota(const int& x = 10) {
-    std::vector<int> indices(x);
-    std::ranges::iota(indices.begin(), indices.end(), 0);
-    return Rcpp::wrap(indices);
+
+    #if __cplusplus == 202302L
+        Rcpp::Rcout << "Hello from C++23" << std::endl;
+        std::vector<int> indices(x);
+        std::ranges::iota(indices.begin(), indices.end(), 0);
+        return Rcpp::wrap(indices);
+    #endif
+
+    #if __cplusplus == 202002L
+        Rcpp::Rcout << "Hello from C++20" << std::endl;
+        std::vector<int> indices(x);
+        std::iota(indices.begin(), indices.end(), 0);
+        return Rcpp::wrap(indices);
+    #endif
+
+    #if __cplusplus == 201703L
+        Rcpp::Rcout << "Hello from C++17" << std::endl;
+        std::vector<int> indices(x);
+        std::iota(indices.begin(), indices.end(), 0);
+        return Rcpp::wrap(indices);
+    #endif
 }
